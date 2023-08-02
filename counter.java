6b20d2f5dfb59018d94e5416898d6c35c58c8ca5
Anton <optimavar310am@gmail.com>
2023-08-02 16:21:44 +0300

    trywith eto takaya toska

--- counter.java
+++ counter.java
@@ -1,8 +1,12 @@
 /**
- * counter
+ * за_утомилс_я уже честно говоряяяяяя
+ * но раз уж взялся то можно сделать хорошо :\
+ * шо вообще такое этот трайвиз. Избавляет нас от необходимости закрывать файл. 
+ * Значит надо значение брать из файла, и из-за этого вся малина. Тоска.
+ * 
  */
 
-public class counter {
+public class Counter {
     private Integer value = null;
 
     private static final Integer default_value = 0;
@@ -11,12 +15,15 @@ public class counter {
         return value;
     }
 
-    public counter() {
-        this.value = counter.default_value;
+    public Counter() {
+        this.value = Counter.default_value;
+    }
+    public void  add (){
+        throw new RoutineViolationException("Ну надо так надо :\\");
     }
     
-    public void increment() throws RoutineViolationException{
-
-        throw new RoutineViolationException("Мы так делать не будем.");
+    public void increment(){
+        this.value ++;
+        
     }
 }
